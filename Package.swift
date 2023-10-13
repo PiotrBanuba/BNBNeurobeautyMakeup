dd2c181da13422e808ca5c63ed605ca159deea08
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 09:52:06 +0300

    1.8.0-10

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-9"
+let bnbPackageVersion: Version = "1.8.0-10"
 
 let package = Package(
     name: "BNBNeurobeautyMakeup",
@@ -13,10 +13,10 @@ let package = Package(
             name: "BNBNeurobeautyMakeup",
             targets: [
                 "BNBNeurobeautyMakeup",
-                "BNBNeurobeautyMakeupBNBSdkCore",
-                "BNBNeurobeautyMakeupBNBEffectPlayer",
-                "BNBNeurobeautyMakeupBNBScripting",
-                "BNBNeurobeautyMakeupBNBFaceTracker"
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting",
+                "BNBFaceTracker"
             ]
         ),
     ],
@@ -43,42 +43,6 @@ let package = Package(
             name: "BNBNeurobeautyMakeup",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBNeurobeautyMakeup.zip",
             checksum: "1c959c056d30fa942bea6077a250b6477b01404b7598e5164818c1045d0463ad"
-        ),
-        .target(
-            name: "BNBNeurobeautyMakeupBNBSdkCore",
-            dependencies: [
-                .product(
-                    name: "BNBSdkCore",
-                    package: "BNBSdkCore"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBNeurobeautyMakeupBNBEffectPlayer",
-            dependencies: [
-                .product(
-                    name: "BNBEffectPlayer",
-                    package: "BNBEffectPlayer"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBNeurobeautyMakeupBNBScripting",
-            dependencies: [
-                .product(
-                    name: "BNBScripting",
-                    package: "BNBScripting"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBNeurobeautyMakeupBNBFaceTracker",
-            dependencies: [
-                .product(
-                    name: "BNBFaceTracker",
-                    package: "BNBFaceTracker"
-                ),
-            ]
         ),
     ]
 )
